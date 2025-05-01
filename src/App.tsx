a972e8175e8b2ac80b10789d35110ed15764ec03
TMK-git <manikantathandra863@gmail.com>
2025-05-01 18:01:03 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,29 @@
 import React from 'react';
-import {  HashRouter as Router  } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 import Home from './pages/Home';
 import QandA from './pages/QandA';
 import Quiz from './pages/Quiz';
 import Interactive from './pages/Interactive';
-import { AIProvider } from './context/AIContext';
-import { ThemeProvider } from './context/ThemeContext';
-import { AuthProvider } from './context/AuthContext';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <ThemeProvider>
-      <AuthProvider>
-        <AIProvider>
-          <Router>
-            <div className="flex flex-col min-h-screen bg-slate-50 dark:bg-gray-900 transition-colors duration-200">
-              <Header />
-              <main className="flex-grow container mx-auto px-4 py-8">
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/qanda" element={<QandA />} />
-                  <Route path="/quiz" element={<Quiz />} />
-                  <Route path="/interactive" element={<Interactive />} />
-                </Routes>
-              </main>
-              <Footer />
-              <Toaster position="bottom-right" />
-            </div>
-          </Router>
-        </AIProvider>
-      </AuthProvider>
-    </ThemeProvider>
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-grow p-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/qanda" element={<QandA />} />
+            <Route path="/quiz" element={<Quiz />} />
+            <Route path="/interactive" element={<Interactive />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;

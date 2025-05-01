a7973c79a4b5eff2b195bb21f8022755880989f8
TMK-git <manikantathandra863@gmail.com>
2025-05-01 18:11:11 +0530

    Create Navbar.tsx

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="bg-blue-600 p-4">
+      <div className="container mx-auto">
+        <h2 className="text-white text-xl">Learnmate AI</h2>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
